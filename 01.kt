46de640afa9ef3234c2bdab70152b0081db3851a
Alexander Golovanov <Golovanov12345@gmail.com>
2022-12-02 00:25:35 +0300

    Move .split functions to the separate file

--- 01.kt
+++ 01.kt
@@ -1,39 +1,5 @@
 import java.io.File
 
-fun <T> List<T>.split(x: T): List<List<T>> {
-    val cur = mutableListOf<T>()
-    val res = mutableListOf<List<T>>()
-    for (y in this) {
-        if (y == x) {
-            res.add(cur.toList())
-            cur.clear()
-        } else {
-            cur.add(y)
-        }
-    }
-    if (cur.isNotEmpty()) {
-        res.add(cur.toList())
-    }
-    return res
-}
-
-fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> {
-    val cur = mutableListOf<T>()
-    val res = mutableListOf<List<T>>()
-    for (y in this) {
-        if (predicate(y)) {
-            res.add(cur.toList())
-            cur.clear()
-        } else {
-            cur.add(y)
-        }
-    }
-    if (cur.isNotEmpty()) {
-        res.add(cur.toList())
-    }
-    return res
-}
-
 fun solveEasy(lines: List<String>) {
     val a = lines.split("")
     println(a.map { it.sumOf(String::toInt) }.maxOrNull()!!)
